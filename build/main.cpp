b2222c1de69af4f77af41a288c3b6fb7ed508b69
hjklaswd <DiegoLuo@163.com>
2020-06-16 16:42:15 +0800

    first commit

--- build/main.cpp
+++ build/main.cpp
@@ -1,11 +1,33 @@
+#include <QApplication>
+#include <QDir>
+#include <QFileInfo>
 #include "mainwidget.h"
 #include "mysqldb.h"
-#include <QApplication>
+#define XMLFILENAME "info.xml"
 
-
+void createXml(QString fileName)
+{
+    QFile xmlFile(fileName);
+    qDebug()<<fileName << " &&&&&&&&&&&&&& ";
+    xmlFile.open(QIODevice::WriteOnly | QIODevice::Text);
+    xmlFile.close();
+}
+void getXmlInfo()
+{
+    QString xmlPath = QDir::currentPath();
+    QString xmlFileName = xmlPath + "/" + XMLFILENAME;
+    qDebug()<<xmlFileName << " &&&&&&&&&&&&&& ";
+    QFileInfo xmlInfo(xmlFileName);
+    if(!xmlInfo.exists())
+    {
+        qDebug()<<"no";
+        createXml(xmlFileName);
+    }
+}
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    getXmlInfo();
     mainWidget w;
     w.show();
     return a.exec();
